b22376c1c919681859ceea13a80b7867787d0c21
Eduardo Hertz <eduardohertz@gmail.com>
2011-07-01 12:14:43 -0300

    changed get_current_user method name.

--- lib/include_method.rb
+++ lib/include_method.rb
@@ -1,14 +1,13 @@
 module ActionController
   class Base
-    before_filter :get_current_user
+    before_filter :_get_current_user
 
-    def get_current_user
-      puts "######## 1"
+    def _get_current_user
     end
 
     def self.before_filter *names, &blk
       super *names, &blk
-      ActionController::Base.set_callback(:process_action, :get_current_user)
+      ActionController::Base.set_callback(:process_action, :_get_current_user)
     end
 
   end
--- spec/include_method_spec.rb
+++ spec/include_method_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 describe ActionController::Base, :type => :controller do
   it 'should respond_to get_current_user' do
-    ActionController::Base.new.respond_to?(:get_current_user).should be_true
+    ActionController::Base.new.respond_to?(:_get_current_user).should be_true
   end
 
   it 'should put :get_current_user in the of chain filter' do
     ActionController::Base.before_filter :some_other_method
-    ActionController::Base._process_action_callbacks.map(&:filter).should == [:some_other_method, :get_current_user]
+    ActionController::Base._process_action_callbacks.map(&:filter).should == [:some_other_method, :_get_current_user]
   end
 
 end
